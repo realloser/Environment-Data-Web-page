63aa2c8c553140e0370a26bd1b0c97000763f7a3
Martyn Messerli <realloser@gmx.ch>
2018-09-09 18:21:26 +0200

    Show nodes count

--- src/components/Environment/Source.jsx
+++ src/components/Environment/Source.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 
+import SourceNodes from './SourceNodes.jsx';
+
 @observer
 class Source extends React.Component {
    constructor(props) {
@@ -8,11 +10,14 @@ class Source extends React.Component {
    }
 
    render() {
-      const { source, display_name } = this.props.source;
+      const { sourceName, display_name } = this.props.source;
       return (
-         <li key={source} style={{ display: 'block' }}>
+         <li key={sourceName} style={{ display: 'block' }}>
             <div className='title nodes'>
                <h4>{ display_name }</h4>
+            </div>
+            <div className='nodes'>
+               <SourceNodes source={this.props.source} />
             </div>
          </li>
       )
---
+++ src/components/Environment/SourceNodes.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+
+@observer
+class SourceNodes extends React.Component {
+      constructor(props) {
+            super(props);
+            this.state = {
+                  nodes: [],
+                  fetchState: 'initialized',
+            }
+      }
+      componentDidMount() {
+            this.setState({ fetchState: 'fetching' });
+            const nodesUrl = this.props.source.relations.nodes.href;
+            fetch(nodesUrl)
+                  .then((response) => {
+                        if (response.ok) {
+                              return response.json();
+                        }
+                        throw new Error('Network response was not ok.');
+                  })
+                  .then(json => {
+                        this.setState({ nodes: json.data[0].nodes, fetchState: 'done' })
+                  })
+                  .catch(e => {
+                        this.setState({ fetchState: 'failed', error: e });
+                  });
+      }
+
+      render() {
+            const { nodes, fetchState, error } = this.state;
+
+            const renderNode = (node) => {
+                  return node.name
+            }
+
+            switch (fetchState) {
+                  case 'initialized':
+                        return <span>initialized.</span>
+                  case 'fetching':
+                        return <span>Fetching nodes...</span>
+                  case 'failed':
+                        return (
+                              <div>
+                                    <span className={'error'}>Failed to fetch the data:</span>
+                                    <span className={'errorDetails'}>error</span>
+                              </div>
+                        )
+                  case 'done':
+                        return (
+                              <div>
+                                    <span className="section-header">{nodes.length} Nodes</span>
+                                    <ul>{nodes.map((node) => renderNode(node))}</ul>
+                              </div>
+                        )
+                  default:
+                        console.error('invalid state:', fetchState)
+                        return (
+                              <div>
+                                    <span className={'error'}>Implementation error</span>
+                                    <span className={'errorDetails'}>unknown state</span>
+                              </div >
+                        )
+
+            }
+
+      }
+}
+
+export default SourceNodes
